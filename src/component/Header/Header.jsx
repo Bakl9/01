fd199cb6ebe3dce24a33d0d23d785a3869453283
Makarov ilya <74796903+Bakl9@users.noreply.github.com>
2021-09-25 11:45:57 +0300

    Update Header.jsx

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -5,6 +5,6 @@ export const  Header = () => {
     return(
         <header className={s.over}>
         <img src='https://upload.wikimedia.org/wikipedia/commons/5/59/Logo-Logo.svg'></img>
-      </header> 
+      </header>  
     )
 }   
